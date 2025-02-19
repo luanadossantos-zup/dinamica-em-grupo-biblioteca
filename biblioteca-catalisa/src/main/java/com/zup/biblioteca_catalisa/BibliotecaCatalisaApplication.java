95d6f7dd3a9fc7c0331fa51375a94b0d3675993b
Luana Silva <luana.dossantos@zup.com.br>
2025-02-19 14:22:05 -0300

    chore: Initial commit with setup

---
+++ biblioteca-catalisa/src/main/java/com/zup/biblioteca_catalisa/BibliotecaCatalisaApplication.java
@@ -0,0 +1,13 @@
+package com.zup.biblioteca_catalisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaCatalisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaCatalisaApplication.class, args);
+	}
+
+}
---
+++ biblioteca-catalisa/src/test/java/com/zup/biblioteca_catalisa/BibliotecaCatalisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.biblioteca_catalisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaCatalisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
